f4a87794885ac346016cbd8df6137ccbf83ffb5f
nkongenelly <nkongenelly94@gmail.com>
2019-03-29 13:03:08 +0300

    gitignore config variables

--- readTweets/config.py
+++ readTweets/config.py
@@ -1,5 +1,5 @@
 #variables that contain user credentials to acess Twitter API
-consumer_key = 'Loy2EJVTB0G0eGjJEO3aTCODJ'
-consumer_secret = 'vHx40p0IktFfZr0fJKJt0pvkrJPN7CYmQjviYblJms3mxIiz1K'
-access_token = '923908720249229313-np5cLiRK66owI4PtU7FoYLR20KSu62u'
-access_secret = 'QRdvChPGJ6em9tPkBJa0eTC34f78Z4xwMZrLAgQ91vTL8'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
