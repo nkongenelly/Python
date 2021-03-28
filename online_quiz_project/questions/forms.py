06f5067bb7acce20ae7453ec9f02b4cd57fc1021
nkongenelly <nkongenelly94@gmail.com>
2021-03-28 20:38:28 +0530

    Added questions category and level

---
+++ online_quiz_project/questions/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class QuestionForms(forms.Form):
+    CATEGORIES = (("Any Category", "Any Category"),("General Knowledge", "General Knowledge"), ("Science & Nature", "Science & Nature"), ("Science: Mathematics", "Science: Mathematics"),
+     ("Science: Computer", "Science: Computer"), ("Geography", "Geography"), ("History", "History"), ("Politics", "Politics"), ("Art", "Art"), ("Animals", "Animals"))
+    category = forms.ChoiceField(choices=CATEGORIES)
+    LEVELS = (("Any Difficulty", "Any Difficulty"), ("easy", "easy"), ("Medium", "Medium"), ("hard", "hard"))
+    level = forms.ChoiceField(choices=LEVELS)
+
+    class Meta:
+        fields = ["category", "level"]
--- online_quiz_project/questions/views.py
+++ online_quiz_project/questions/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import QuestionForms
 
+categories = ["Any Category", "General Knowledge", "Science & Nature", "Science: Mathematics", "Science: Computer", "Geography", "History", "Politics", "Art", "Animals"]
+difficulty = ["Any Difficulty", "easy", "Medium", "hard"]
+category = ""
+level = ""
 def home(request):
-    return render(request, 'questions/home.html')
+    context = {
+        "categories": categories,
+            "difficulty": difficulty
+        }
+    print("request.method = ", request.method)
+    if request.method == 'POST':
+        category1 = request.POST
+        print("categoriessss = ", category1)
+        formValues = QuestionForms(request.POST)
+        print("formValues.is_valid() = ", formValues.is_valid())
+        if formValues.is_valid():
+            category = formValues.cleaned_data.get("category")
+            level = formValues.cleaned_data.get("level")
+            print("category = ", category)
+            print("level = ", level)
+            return render(request, 'questions/questions.html', {'form': formValues}) 
+
+    else :
+        formValues = QuestionForms()
+        context['form']=formValues
+        #     return render(request, 'questions/home.html', {'form': formValues})
+    return render(request, 'questions/home.html', {'form': formValues})
 
 def questions(request):
+    print("request.method 2= ", request.method)
     return render(request, 'questions/questions.html')
